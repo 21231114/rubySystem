77b7ca315ce408dee7bd1a5418979354fa03c3c1
root <root@SC-202005091355>
2023-11-24 20:28:45 +0800

    更改了一点，但仍存在bug

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -11,20 +11,19 @@ class User < ApplicationRecord
   validates :num, presence: true
   validate :status_match # 学工号和身份要匹配，学工号位数合法
 
-  has_many :relations,dependent: :destroy
+  has_many :relations, dependent: :destroy
   has_many :subjects, through: :relations
   def status_match
-    if num.length != 8 && num.length!=6
-      errors.add('学工号', '应为6位或8位的数字')
+    if User.find_by(num: num)
+      errors.add('学工号','已被注册')
     end
+    errors.add('学工号', '应为6位或8位的数字') if num.length != 8 && num.length != 6
     num.chars.each do |c|
-      if c>'9'|| c<'0'
-        errors.add('学工号', '应为6位或8位的数字')
-      end
+      errors.add('学工号', '应为6位或8位的数字') if c > '9' || c < '0'
     end
-    if (num.length == 8&&status=="teacher")||(num.length==6&&status=="student")
-      errors.add('身份', '与学工号不匹配')
-    end
+    return unless (num.length == 8 && status == 'teacher') || (num.length == 6 && status == 'student')
+
+    errors.add('身份', '与学工号不匹配')
   end
 
   # 对默认方法进行重写
