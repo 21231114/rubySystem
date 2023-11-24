9abaca30b4bac67370708bdf0ff7a8e33020cc2f
root <root@SC-202005091355>
2023-11-24 12:17:34 +0800

    增加身份验证功能

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -2,22 +2,32 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable#, skip: [:email_validation]
+         :recoverable, :rememberable, :validatable # , skip: [:email_validation]
   has_many :subjects
-  has_many :discussions
+  has_many :discussions, dependent: :destroy
   attr_accessor :signin # 虚拟属性
 
   validates :username, presence: true
   validates :status, presence: true
   validates :num, presence: true
+  validate :status_match # 学工号和身份要匹配，学工号位数合法
 
   has_many :relations
-  has_many :subjects ,through: :relations
+  has_many :subjects, through: :relations
+  def status_match
+    if num.length != 8 && num.length!=6
+      errors.add('学工号', '形式不合法')
+    end
+    if (num.length == 8&&status=="teacher")||(num.length==6&&status=="student")
+      errors.add('身份', '与学工号不匹配')
+    end
+  end
 
   # 对默认方法进行重写
   def email_required?
     false
   end
+
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if signin = conditions.delete(:signin)
